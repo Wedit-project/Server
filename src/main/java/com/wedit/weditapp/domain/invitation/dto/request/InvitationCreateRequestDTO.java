6f4822f670091cb0365ba6bf3de22235610c45b4
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-01-10 00:56:56 +0900

    Invitation create dto 생성

---
+++ src/main/java/com/wedit/weditapp/domain/invitation/dto/request/InvitationCreateRequestDTO.java
@@ -0,0 +1,44 @@
+package com.wedit.weditapp.domain.invitation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+public class InvitationCreateRequestDTO {
+    @NotBlank(message = "groom cannot be blank")
+    private String groom; // 신랑 이름
+
+    @NotBlank(message = "bride cannot be blank")
+    private String bride; // 신부 이름
+
+    private String groomF; // 신랑 아버지 이름
+
+    private String groomM;  // 신랑 어머니 이름
+
+    private String brideF; // 신부 아버지 이름
+
+    private String brideM; // 신부 어머니 이름
+
+    @NotBlank(message = "address cannot be blank")
+    private String address; // 주소
+
+    private String extraAddress; // 상세 주소
+
+    @NotBlank(message = "date cannot be blank")
+    private LocalDate date; // 결혼식 날짜
+
+    @NotBlank(message = "theme cannot be blank")
+    private String theme; // 테마
+
+    private String distribution; // 청첩장 URL
+
+    private boolean guestBookOption; // 방명록 옵션
+
+    private boolean decisionOption; // 참석 의사 수집 옵션
+
+    private boolean accountOption; // 계좌 공개 옵션
+}
