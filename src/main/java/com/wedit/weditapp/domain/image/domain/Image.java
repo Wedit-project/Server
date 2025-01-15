d2bc73d8722a0c5b4b639b09a307b127849569ed
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-01-16 06:35:58 +0900

    [feat] #15 - domain 생성

---
+++ src/main/java/com/wedit/weditapp/domain/image/domain/Image.java
@@ -0,0 +1,53 @@
+package com.wedit.weditapp.domain.image.domain;
+
+import com.wedit.weditapp.domain.invitation.domain.Invitation;
+import com.wedit.weditapp.domain.shared.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "images")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(nullable = false)
+	private String url;
+
+	@Column(nullable = false)
+	private int location;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "invitation_id", nullable = false)
+	private Invitation invitation;
+
+	@Builder
+	private Image(String url, int location, Invitation invitation) {
+		this.url = url;
+		this.location = location;
+		this.invitation = invitation;
+	}
+
+	public Image createImage(String url, int location, Invitation invitation) {
+		return Image.builder()
+			.url(url)
+			.location(location)
+			.invitation(invitation)
+			.build();
+	}
+}
