9e5e2b862ae99f89f66f1fdc1ac806db8e7a327e
dogsub <ehdtjq0909@gmail.com>
2025-01-19 04:15:56 +0900

    [fix] #17 SecurityConfig 수정

--- src/main/java/com/wedit/weditapp/global/config/SecurityConfig.java
+++ src/main/java/com/wedit/weditapp/global/config/SecurityConfig.java
@@ -1,26 +1,24 @@
 package com.wedit.weditapp.global.config;
 
-import static org.springframework.security.config.Customizer.*;
-
-import java.util.Arrays;
-
+import com.wedit.weditapp.global.auth.login.handler.OAuth2LoginFailureHandler;
+import com.wedit.weditapp.global.auth.login.handler.OAuth2LoginSuccessHandler;
+import com.wedit.weditapp.global.auth.login.service.CustomOAuth2UserService;
 import com.wedit.weditapp.global.auth.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import lombok.RequiredArgsConstructor;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -28,33 +26,42 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+	private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.formLogin(AbstractHttpConfigurer::disable)
-			.httpBasic(AbstractHttpConfigurer::disable)
-			.cors(withDefaults())
-			.csrf(AbstractHttpConfigurer::disable)
+				// 1. 폼로그인 / httpBasic / CSRF 비활성화
+				.formLogin(AbstractHttpConfigurer::disable)
+				.httpBasic(AbstractHttpConfigurer::disable)
+				.csrf(AbstractHttpConfigurer::disable)
 
-			// 세션 관련 정책 추가 : 세션 방식 사용 X (오직 JWT만 사용)
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				// 2. 세션 관련 정책 추가 : 세션 방식 사용 X (오직 JWT만 사용)
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-			.headers(
-				headersConfigurer ->
-					headersConfigurer
-						.frameOptions(
-							HeadersConfigurer.FrameOptionsConfig::sameOrigin
+				// 3. 권한 설정
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/v3/api-docs/**",
+								"/swagger-ui/**",
+								"/api/members/login",
+								"/api/members/signup",
+								"/oauth2/**")
+						.permitAll()
+						.anyRequest().authenticated()
+				)
+
+				// 4. OAuth2 설정
+				.oauth2Login(oauth2 -> oauth2
+						.userInfoEndpoint(userInfo ->
+								userInfo.userService(customOAuth2UserService)
 						)
-			)
-			.authorizeHttpRequests(
-				authorize ->
-					authorize
-						.requestMatchers( "/v3/api-docs/**", "/swagger-ui/**", "/api/members/login", "/api/members/signup").permitAll() // swagger-ui 접근 허용 + 로그인과 회원가입까지만 허용
-						.anyRequest().authenticated() // 그외에는 Access Token을 가진 유저만 접근 가능
-			);
+						.successHandler(oAuth2LoginSuccessHandler)
+						.failureHandler(oAuth2LoginFailureHandler)
+				);
 
-		// JWT 인증 필터 등록
+		// 5. JWT 인증 필터 등록
 		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
@@ -74,11 +81,4 @@ public class SecurityConfig {
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
-
-	// 회원가입 시 사용
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
-
 }
