6b2f76e7d96df8d28d593f2990579bb8d7d5c81d
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-01-19 00:51:27 +0900

    [fix] #15 - InvitationController 수정

--- src/main/java/com/wedit/weditapp/domain/invitation/controller/InvitationController.java
+++ src/main/java/com/wedit/weditapp/domain/invitation/controller/InvitationController.java
@@ -1,13 +1,20 @@
 package com.wedit.weditapp.domain.invitation.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.wedit.weditapp.domain.invitation.domain.Invitation;
 import com.wedit.weditapp.domain.invitation.dto.request.InvitationCreateRequestDTO;
 import com.wedit.weditapp.domain.invitation.service.InvitationService;
+import com.wedit.weditapp.domain.member.domain.Member;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,10 +26,19 @@ public class InvitationController {
 	private final InvitationService invitationService;
 
 	@PostMapping
-	public ResponseEntity<String> createInvitation(@Valid @ModelAttribute InvitationCreateRequestDTO dto) {
-		// 초대장 생성
-		Long invitationId = invitationService.createInvitation(dto);
+	public ResponseEntity<Long> createInvitation(
+		@RequestParam("images") List<MultipartFile> images,
+		@Validated @ModelAttribute InvitationCreateRequestDTO request
+	) {
+		// Member 객체는 인증 정보를 통해 가져와야 함
+		Member member = getAuthenticatedMember();
 
-		return ResponseEntity.ok("Invitation created successfully with ID: " + invitationId);
+		Invitation invitation = invitationService.saveInvitationWithImages(member, request, images);
+		return ResponseEntity.ok(invitation.getId());
+	}
+
+	private Member getAuthenticatedMember() {
+		// TODO: 인증된 사용자 정보를 가져오는 로직 구현
+		return new Member();
 	}
 }
