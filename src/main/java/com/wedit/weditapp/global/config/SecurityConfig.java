2790784ef6000225ae3efb429f13fadc16b20861
Dongyun Kim <djskaldrjfj@naver.com>
2025-02-09 19:59:14 +0900

    [fix] cors 변경

--- src/main/java/com/wedit/weditapp/global/config/SecurityConfig.java
+++ src/main/java/com/wedit/weditapp/global/config/SecurityConfig.java
@@ -30,9 +30,8 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	//@Value("#{'${cors.allowed-origins}'.split(',')}")
-	/*@Value("${cors.allowed-origins}")
-	private String[] allowedOrigins;*/
+	@Value("#{'${cors.allowed-origins}'.split(',')}")
+	private String[] allowedOrigins;
 
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	private final CustomOAuth2UserService customOAuth2UserService;
@@ -83,15 +82,14 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 
-		//configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
-		configuration.setAllowedOrigins(Arrays.asList(
+		configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+		/*configuration.setAllowedOrigins(Arrays.asList(
 			"http://localhost:3000",
 			"http://localhost:5173",
 			"http://localhost:8080",
 			"https://wedit.site",
-			"https://43.201.85.194.nip.io",
 			"https://wedit.site/oauth/callback/kakao"
-			));
+			));*/
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setExposedHeaders(Arrays.asList("Content-Type", "Authorization", "Authorization-refresh", "accept"));
